1cfe96648c0028702cc7d4fca6057f9b4e25a9e4
M Rusydi Arif Zakaria <muhamadrusydiarif.za@petronas.com>
2023-01-29 14:18:39 +0800

    code push

--- RemoteCodingTest/Program.cs
+++ RemoteCodingTest/Program.cs
@@ -9,32 +9,53 @@ namespace RemoteCodingTest
             string? options;
             string? inputCalc;
 
-            for (int i = 0; i == -1; i++)
+            Console.WriteLine("Welcome to Calculating System");
+            for (int i = 0; i < 10; i--)
             {
-                Console.WriteLine("Please enter a calculation:");
+                toSleep();
+                Console.WriteLine("Please enter a calculation (eg 1 + 1): ");
                 inputCalc = Console.ReadLine();
-                if (string.IsNullOrEmpty(inputCalc))
-                {
-                    Console.WriteLine("Please enter some calculation:");
-                    inputCalc = Console.ReadLine();
 
-                    if (string.IsNullOrEmpty(inputCalc)) continue;
+                if (string.IsNullOrWhiteSpace(inputCalc))
+                    continue;
+                else
+                {
+                    Console.WriteLine("The answer for " + inputCalc + ": " + Calculate(inputCalc));
+                    toSleep(1500);
+
+                TryAgain:
+                    Console.WriteLine("Do you want to try again? y/n");
+                    options = Console.ReadLine();
+
+                    if (options == "y")
+                        continue;
+                    else if (options == "n") 
+                    {
+                        toSleep();
+                        Console.WriteLine("Thank you for using Calculating System");
+                        break;
+                    }
                     else
                     {
-                        Console.WriteLine(Calculate(inputCalc));
-                        Thread.Sleep(1000);
-                        Console.WriteLine("Do you want to try again? y/n");
-                        options = Console.ReadLine() == null ? null : Console.ReadLine().ToLower();
-                        if (options == "y")  
+                        toSleep();
+                        Console.WriteLine("Invalid Input");
+                        toSleep(1500);
+                        goto TryAgain;
                     }
                 }
-                else break;
             }
         }
         public static double Calculate(string sum)
         {
 
             return 0;
+        }
+
+        public static void toSleep(int? millisec = null)
+        {
+            int? interval = millisec == null ? 1000 : millisec;
+            Console.WriteLine("Please Wait...");
+            Thread.Sleep((int)interval);
         }
 
     }
